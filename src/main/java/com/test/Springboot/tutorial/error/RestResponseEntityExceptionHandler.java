c625685dd8e3bd7c6f0eba3b7d19d5ee7a7c8587
GuptaAdarsh10 <123458574+GuptaAdarsh10@users.noreply.github.com>
2024-05-04 20:11:29 +0530

    Initial commit

---
+++ src/main/java/com/test/Springboot/tutorial/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.test.Springboot.tutorial.error;
+
+import com.test.Springboot.tutorial.entity.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@ResponseStatus
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(com.test.Springboot.tutorial.error.DepartmentNotFoundException.class)
+    public ResponseEntity<ErrorMessage> departmentNotFoundException(com.test.Springboot.tutorial.error.DepartmentNotFoundException exception,
+                                                                    WebRequest request) {
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
+                exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(message);
+    }
+}
---
+++ src/main/java/com/test/Springboot/tutorial/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.test.Springboot.tutorial.service;
+
+import com.test.Springboot.tutorial.entity.Department;
+import com.test.Springboot.tutorial.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    Department fetchDepartmentByName(String departmentName);
+}
